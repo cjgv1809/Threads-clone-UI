42e777feb56dac39a185d08f3be0c385d04d9455
Carlos Gomes <carlos_gomes1809@hotmail.com>
2023-09-27 22:22:21 -0300

    add initial setup and types

---
+++ typings.d.ts
@@ -0,0 +1,31 @@
+interface Thread {
+  id: string;
+  author: User;
+  content: string;
+  image?: string;
+  replies?: Reply[];
+  repliesCount: number;
+  likesCount: number;
+  mention?: boolean;
+  mentionUser?: User;
+  createdAt: string;
+}
+
+interface Reply {
+  id: string;
+  author: User;
+  content: string;
+  likes: number;
+  createdAt: string;
+}
+
+interface User {
+  id: string;
+  name: string;
+  username: string;
+  verified: boolean;
+  photo: string;
+  bio: string;
+  link?: string;
+  followers?: User[];
+}
